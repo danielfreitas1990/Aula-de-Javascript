265b86e420cf1b02a2a5073f3b7d5b7bc2eaf61e
dhanielfreitas@gmail.com <santosfc01>
2020-06-16 20:36:44 -0300

    Concluído
    Aula de Javascrip.

---
+++ JS/main.js
@@ -0,0 +1,95 @@
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar")
+}
+
+function redirecionar(){
+    window.open("http://neotarget.com.br/");
+    //window.location.href = "http://neotarget.com.br/";
+}
+
+function trocar(){
+    document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+*/
+
+/*
+var validar = 0;
+function validadeIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade")
+validarIdade(idade);
+console.log(validar);
+
+alert(soma(5, 10));
+*/
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for(count=0; count<= 5; count++){
+    alert(count);
+};
+*/
+
+//var count = 0;
+//while (count < 5){
+//    console.log(count);
+//    count = count + 1;
+//};
+
+//var idade = prompt("Qual sua idade");
+//if (idade >= 18){
+//    alert("maior de idade");
+//}else{
+//    alert("menor de idade");
+//    }
+
+//var frutas = [{nome:"maça", cor: "vermelha"}, {nome:"uva",cor:"roxa"}];
+//console.log(frutas);
+//alert(frutas[1].nome);
+
+//var lista = ["maça", "pera" , "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString())
+//console.log(lista.join(" - "))
+
+
+//var nome = "Daniel Freitas";
+//var idade = 29;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos ")
+//alert(idade + idade2");
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toLocaleLowerCase);
+//alert(frase.replace("Japão","Brasil"));
+
+
